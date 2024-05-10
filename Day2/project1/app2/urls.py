2029341448ba0872fec7c2f6007603ad32724da4
Siva Balan V <113979953+sivasb1019@users.noreply.github.com>
2024-05-10 17:32:31 +0530

    Add files via upload
    Day2 - May 10, 2024
    1. Finished creating multiple Django applications and URLs. Employed Python, Django, and HTML technologies for development.
    2. Added new logic funtions for multiple urls in views and successfully ran our application on the server.

---
+++ Day2/project1/app2/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import greetings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('greetings/',greetings)
+]
